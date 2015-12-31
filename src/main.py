0e87b2315c088c0924e9b20bc85c09fc00e33aab
danidee10 <osaetindaniel@gmail.com>
2015-12-31 16:31:07 +0100

    fixed some issues with the main program flow

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from hotspot import Hotspot
 
 def main():
 
-    #function to check if exit was entered anywhere, exit the program with 0 as the exit status
+    # method to check if exit was entered anywhere, exit the program with 0 as the exit status
     def check_exit(input_to_check):
         if input_to_check == "exit":
             sys.exit(0)
@@ -21,37 +21,36 @@ def main():
         if yes == "yes":
             hotspot = Hotspot()
             hotspot.start()
+            break
         # capture the username and password from the user
         elif yes == "no":
             name = input("Enter the Name of the New network:\n")
+            password = ""
             check_exit(name)
-            while True:
+            while len(password) < 8:
                 password = input("""Enter the password for the network
 hint: password must be at least eight characters: """)
-                check_exit(password)
-                if len(password) >= 8:
-                    hotspot = Hotspot(name, password)
-                    hotspot.initialize_hotspot()
-                    hotspot.start()
-                    break
-                else:
-                    continue
-
-                    while True:
-                        stop = input("type stop to stop the wireless network")
-                        stop.lower()
-                        if stop == "stop":
-                            hotspot.stop()
-                            break
-                        else:
-                            print('invalid option type stop to stop the wireless network')
-                            continue
-
-                    input("Press Enter to exit")
-
+            hotspot = Hotspot(name, password)
+            hotspot.initialize_hotspot()
+            hotspot.start()
+            break
         else:
             print("Invalid option please enter Yes or No:\n")
             continue
+
+
+    # prompt the user to end the active hotspot session
+    while True:
+        stop = input("type stop to stop the wireless network:\n")
+        stop.lower()
+        if stop == "stop":
+            hotspot.stop()
+            # should modify the check_exit function to accept an optional argument instead of using sys
+            sys.exit(0)
+        else:
+            print('invalid option type stop to stop the wireless network:\n')
+            continue
+
 
 
 if __name__ == "__main__": main()
